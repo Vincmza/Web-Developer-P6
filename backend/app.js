61960ddd717b7cc1f69e1d9b6bd676560bf5ea81
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-09-01 17:42:31 +0200

    app.js fixed headers in relation of cors issues

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const app = express();
-
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
@@ -14,13 +12,12 @@ mongoose.connect('mongodb+srv://Vincent:arkhoninfaustus1987@cluster0.ifemo.mongo
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-  });
+});
   
-
 app.use(express.json());
 
 app.use('/api/sauces', saucesRoutes);
