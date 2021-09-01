70aa94717297e48b22a81e8f9e91491bb450fd21
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-09-01 18:51:51 +0200

    authentification_feature set up. middleware folder created

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,13 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
-
 const ctrlSauces = require('../controllers/sauces');
+const auth = require('../middleware/auth');
 
-router.post('/', ctrlSauces.createSauce);
-router.get('/', ctrlSauces.getAllSauces);
-router.get('/:id', ctrlSauces.getOneSauce);
-router.put('/:id', ctrlSauces.modifySauce);
-router.delete('/:id', ctrlSauces.deleteSauce);
+router.post('/', auth, ctrlSauces.createSauce);
+router.get('/', auth, ctrlSauces.getAllSauces);
+router.get('/:id', auth, ctrlSauces.getOneSauce);
+router.put('/:id', auth, ctrlSauces.modifySauce);
+router.delete('/:id', auth, ctrlSauces.deleteSauce);
 
 module.exports = router;
