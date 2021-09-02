5419ed7a71722ac42226612dc1dabe731bde93dc
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-09-02 14:43:24 +0200

    backend_folder:set up multer and middleware file multer-config.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 const mongoose = require('mongoose');
 
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
   
 app.use(express.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ctrlSauces = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, ctrlSauces.createSauce);
+router.post('/', auth, multer, ctrlSauces.createSauce);
 router.get('/', auth, ctrlSauces.getAllSauces);
 router.get('/:id', auth, ctrlSauces.getOneSauce);
 router.put('/:id', auth, ctrlSauces.modifySauce);
