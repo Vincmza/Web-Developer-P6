82262294df2da12bc036f890d057f7aeceec3974
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-09-03 17:51:37 +0200

    backend_folder:set up image modify option in put route

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -6,7 +6,7 @@ const multer = require('../middleware/multer-config');
 
 router.get('/', auth, ctrlSauces.getAllSauces);
 router.get('/:id', auth, ctrlSauces.getOneSauce);
-router.put('/:id', auth, ctrlSauces.modifySauce);
+router.put('/:id', auth, multer, ctrlSauces.modifySauce);
 router.delete('/:id', auth, ctrlSauces.deleteSauce);
 router.post('/', auth, multer, ctrlSauces.createSauce);
 
